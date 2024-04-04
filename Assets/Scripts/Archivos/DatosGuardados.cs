53cb6cae3b6f97afa75a3d5632fa020f44eee745
Pablo Maxiá <pmaxiarodriguez@gmail.com>
2024-04-04 19:56:19 +0200

    Organización carpetas y clase datos guardados

---
+++ Assets/Scripts/Archivos/DatosGuardados.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DatosGuardados
+{
+    [SerializeField] private GameObject archivoGuardado;
+    [SerializeField] private double tiempoJugado;
+    [SerializeField] private float municion;
+}
